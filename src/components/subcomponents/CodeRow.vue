b5a10df45341844bf2ba48da9c3fe0a13849f211
MathyEM <72448225+MathyEM@users.noreply.github.com>
2021-12-26 15:38:24 +0100

    Added subcomponent CodeRow

---
+++ src/components/subcomponents/CodeRow.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="code-row">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CodeRow',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+</style>
