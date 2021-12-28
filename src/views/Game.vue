a3e6b1f06a874987fef8a63281a3a3371b7e7dab
MathyEM <72448225+MathyEM@users.noreply.github.com>
2021-12-28 14:53:04 +0100

    Added HomeMenu.vue

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="game">
+    <HomeMenu msg="Vælg"/>
     <Options msg="⚙"/>
     <Solution msg="Solution"/>
     <Attempts msg="Attempts"/>
     <CodeButtons msg="CodeButtons"/>
-
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import HomeMenu from '@/components/HomeMenu.vue'
 import Options from '@/components/Options.vue'
 import Solution from '@/components/Solution.vue'
 import Attempts from '@/components/Attempts.vue'
@@ -18,6 +19,7 @@ import CodeButtons from '@/components/CodeButtons.vue'
 export default {
   name: 'Game',
   components: {
+    HomeMenu,
     Options,
     Solution,
     Attempts,
