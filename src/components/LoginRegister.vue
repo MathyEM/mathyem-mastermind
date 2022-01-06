14f6507d0f0b7bac88867a4771777cfd2de94807
MathyEM <72448225+MathyEM@users.noreply.github.com>
2022-01-06 10:12:52 +0100

    Added LoginRegister component

---
+++ src/components/LoginRegister.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="login-register">
+    <input v-model="username" type="text" placeholder="Username"><br/>
+    <input v-model="password" type="password" placeholder="Password"><br/>
+    <button @click="login">Opret Rum</button>
+    <button @click="register">Tilslut Rum</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'LoginRegister',
+  props: {
+  },
+	data() {
+		return {
+			username: '',
+      password: '',
+		}
+	},
+	computed: {
+
+  },
+  methods: {
+		login() {
+			console.log('login clicked')
+    },
+    register() {
+			console.log('register clicked')
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+</style>
