780449612a27bd858d0ee134310497fae8893835
MathyEM <72448225+MathyEM@users.noreply.github.com>
2022-02-16 18:09:34 +0100

    More preparations for game status messages

---
+++ src/components/GameStatus.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="game-status">
+    <p>{{ getGameStatus }}</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'GameStatus',
+  components: {
+    
+  },
+  computed: {
+    ...mapGetters(['getSolutionState', 'getCodemaker', 'getRoomUsers', 'getGameStatus']),
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- src/store/modules/statusMessages.js
+++ src/store/modules/statusMessages.js
@@ -5,9 +5,29 @@ const state = {
 
 const getters = {
     getGameStatus: (state, getters, rootState, rootGetters) => {
-        state; getters; rootState; rootGetters
+        state; getters; rootState; rootGetters;
         console.log(rootState.loginStatus)
-        return rootState.loginStatus
+
+        /*  USE TO CALC IF CURRENT USER IS CODEBREAKER/MAKER OR NOT
+            getUsername(id, exclusion) {
+                const users = this.getRoomUsers
+                var user
+
+                if (exclusion) {
+                    user = users.find(function(user) {
+                    return user._id._id !== id
+                    })
+                    return user
+                }
+
+                const user = users.find(function(user) {
+                    return user._id._id === id
+                })
+
+                return user
+            }
+
+        */
     }
 }
 
