8e0bac1709d0588626faaa34fa75f00065340dbd
MathyEM <72448225+MathyEM@users.noreply.github.com>
2022-03-08 18:06:07 +0100

    Only submit form if all input is valid

--- src/components/LoginRegister.vue
+++ src/components/LoginRegister.vue
@@ -119,36 +119,39 @@ export default {
       })
     },
     register() {
-      this.validateRegister(this.$v)
-			// let currentUrl = window.location.pathname
-      // if (this.password.length < 6 || this.username.length < 4) {
-      //   return
-      // }
-      // axios.post(process.env.VUE_APP_SOCKET_ENDPOINT + '/register', {username: this.username, email: this.email, password: this.password}, { withCredentials: true })
-      // .catch((error) => {
-      //   if (error.response) {
-      //     // The request was made and the server responded with a status code
-      //     // that falls out of the range of 2xx
-      //     console.log(error.response.data);
-      //     console.log(error.response.status);
-      //     console.log(error.response.headers);
-      //   } else if (error.request) {
-      //     // The request was made but no response was received
-      //     // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-      //     // http.ClientRequest in node.js
-      //     console.log(error.request);
-      //   } else {
-      //     // Something happened in setting up the request that triggered an Error
-      //     console.log('Error', error.message);
-      //   }
-      // })
-      // .then((response) => {
-      //   if (response) {
-      //     if (response.status == 200) {
-      //       window.location = currentUrl
-      //     }
-      //   }
-      // })
+      if (this.$v.$invalid) {
+        console.log('form invalid')
+        return
+      }
+			let currentUrl = window.location.pathname
+      if (this.password.length < 6 || this.username.length < 4) {
+        return
+      }
+      axios.post(process.env.VUE_APP_SOCKET_ENDPOINT + '/register', {username: this.username, email: this.email, password: this.password}, { withCredentials: true })
+      .catch((error) => {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
+      })
+      .then((response) => {
+        if (response) {
+          if (response.status == 200) {
+            window.location = currentUrl
+          }
+        }
+      })
     }
   }
 }
