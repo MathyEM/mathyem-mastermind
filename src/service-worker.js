67fb8df047ef74132d996096f1d62b5a650c2730
MathyEM <72448225+MathyEM@users.noreply.github.com>
2022-03-18 20:56:29 +0100

    Trying waitUntil again

--- src/service-worker.js
+++ src/service-worker.js
@@ -15,30 +15,26 @@ self.addEventListener('install', () => self.skipWaiting())
 console.log("Service Worker Loaded...1234")
 
 self.addEventListener("push", async event => {
-  try {
-    await localforage.ready()
-    const user = await localforage.getItem('user')
-    const sessionId = await localforage.getItem('sessionId')
-    const sessionValidate = await postData('https://api.mastermind.mem-home.tk/validate-session', {user, sessionId})
-    console.log(sessionValidate)
+  event.waitUntil(
+    localforage.ready().then(async () => {
+      const user = await localforage.getItem('user')
+      const sessionId = await localforage.getItem('sessionId')
 
-    const data = event.data.json()
+      return postData('https://api.mastermind.mem-home.tk/validate-session', {user, sessionId})
+      .then(sessionValidate => {
+        if (sessionValidate.sessionValid) {
+          console.log("Push Recieved...")
+          const data = event.data.json()
 
-    if (sessionValidate.sessionValid) {
-      console.log("Push Recieved...")
-
-      event.waitUntil( // don't terminate the SW untill this is done
-        self.registration.showNotification(data.title, {
-          body: data.body,
-          icon: "http://image.ibb.co/frYOFd/tmlogo.png",
-          vibrate: [200, 200],
-        })
-      )   
-    }
-  } catch (error) {
-    console.error(error)
-  }
-  
+          return self.registration.showNotification(data.title, {
+            body: data.body,
+            icon: "http://image.ibb.co/frYOFd/tmlogo.png",
+            vibrate: [200, 200],
+          })
+        }
+      })
+    })
+  )
 })
 
 async function postData(url = '', data = {}) {
@@ -55,6 +51,6 @@ async function postData(url = '', data = {}) {
     redirect: 'follow', // manual, *follow, error
     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
+  })
+  return response.json() // parses JSON response into native JavaScript objects
 }
