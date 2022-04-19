d1d743290e555668e0310c236f982a2876efc984
MathyEM <72448225+MathyEM@users.noreply.github.com>
2022-04-19 14:18:17 +0200

    Updated next round button to dark theme

--- src/components/subcomponents/NextRoundButton.vue
+++ src/components/subcomponents/NextRoundButton.vue
@@ -29,11 +29,22 @@ export default {
 
 <style scoped lang="scss">
 .next-round {
-  margin: 0.75rem;
+  margin: 0.5rem;
 
   button {
-    font-size: 1.1rem;
+    outline: none;
+    border: none;
     height: 2em;
+    font-size: 1.1rem;
+    padding: 0 0.75rem;
+    border-radius: $button-border-radius;
+    background: $green;
+    color: $text-color;
+    box-shadow: $shadow-2dp;
+    &:active {
+			box-shadow: $shadow-1dp;
+			background: $green-darkened;
+		}
   }
 }
 </style>
