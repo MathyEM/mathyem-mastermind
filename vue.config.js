0f53fb6a68156172ba606c4ff286bd9d108b72ce
MathyEM <72448225+MathyEM@users.noreply.github.com>
2022-04-20 13:22:50 +0200

    new icons

--- vue.config.js
+++ vue.config.js
@@ -27,44 +27,40 @@ module.exports = {
       theme_color: '#121212',
       background_color: '#121212',
       "icons": [{
-        "src": "img/icons/mastermind-logo-dark-48.png",
+        "src": "img/icons/maskable_icon_x48.png",
         "sizes": "48x48",
-        "type": "image/png"
+        "type": "image/png",
+        "purpose": "maskable"
       },
       {
-        "src": "img/icons/mastermind-logo-dark-72.png",
+        "src": "img/icons/maskable_icon_x72.png",
         "sizes": "72x72",
-        "type": "image/png"
+        "type": "image/png",
+        "purpose": "maskable"
       },
       {
-        "src": "img/icons/mastermind-logo-dark-96.png",
+        "src": "img/icons/maskable_icon_x96.png",
         "sizes": "96x96",
-        "type": "image/png"
+        "type": "image/png",
+        "purpose": "maskable"
       },
       {
-        "src": "img/icons/mastermind-logo-dark-144.png",
-        "sizes": "144x144",
-        "type": "image/png"
-      },
-      {
-        "src": "img/icons/mastermind-logo-dark-168.png",
-        "sizes": "168x168",
-        "type": "image/png"
-      },
-      {
-        "src": "img/icons/mastermind-logo-dark-192.png",
+        "src": "img/icons/maskable_icon_x192.png",
         "sizes": "192x192",
-        "type": "image/png"
+        "type": "image/png",
+        "purpose": "maskable"
       },
       {
-        "src": "img/icons/mastermind-logo-dark-256.png",
+        "src": "img/icons/maskable_icon_x386.png",
         "sizes": "256x256",
-        "type": "image/png"
+        "type": "image/png",
+        "purpose": "maskable"
       },
       {
-        "src": "img/icons/mastermind-logo-dark-512.png",
+        "src": "img/icons/maskable_icon_x512.png",
         "sizes": "512x512",
-        "type": "image/png"
+        "type": "image/png",
+        "purpose": "maskable"
       }],
     }
   },
