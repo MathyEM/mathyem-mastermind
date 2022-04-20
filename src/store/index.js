e2a3f03f9f037c43d2022bfbe1f222995fb2c1b1
MathyEM <72448225+MathyEM@users.noreply.github.com>
2022-04-20 13:44:35 +0200

    app version bump

--- src/store/index.js
+++ src/store/index.js
@@ -12,7 +12,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    appVersion: '2.9.3',
+    appVersion: '2.9.4',
     registeringState: false,
     sessionLoading: true,
     user: {
