543f0ce55086ed037b96e96fa4684117690fd1b0
MathyEM <72448225+MathyEM@users.noreply.github.com>
2023-06-22 19:30:10 +0200

    created tutorial button to navigate to tutorial

---
+++ src/components/TutorialButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="tutorial-btn">
+    <button class="btn" @click="$router.push({ name: 'tutorial' })">How to Play</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TutorialButton',
+  components: {
+
+  },
+  props: {
+
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+.tutorial-btn {
+  margin-top: 1rem;
+
+  button {
+    background: $green;
+    color: white;
+  }
+}
+</style>
